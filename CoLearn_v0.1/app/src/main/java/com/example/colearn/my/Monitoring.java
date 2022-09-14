dd069a3a1f794f99c2a51ef403f62514f5c3af8c
su1an <1826179182@qq.com>
2022-09-14 23:57:48 +0800

    优化了实时监控的UI界面

--- CoLearn_v0.1/app/src/main/java/com/example/colearn/my/Monitoring.java
+++ CoLearn_v0.1/app/src/main/java/com/example/colearn/my/Monitoring.java
@@ -1,31 +1,43 @@
 package com.example.colearn.my;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.example.colearn.R;
+import com.example.colearn.chart.Achievement;
+import com.example.colearn.chart.Daily;
+import com.example.colearn.chart.Monthly;
+import com.example.colearn.chart.Weekly;
 import com.example.colearn.databinding.ActivityMonitoringBinding;
 import com.example.colearn.utils.MyPrepareView;
+import com.flyco.tablayout.SlidingTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
 import com.gyf.immersionbar.ImmersionBar;
 
-import retrofit2.http.Url;
+import java.util.ArrayList;
+
 import xyz.doikki.videocontroller.StandardVideoController;
 import xyz.doikki.videocontroller.component.CompleteView;
 import xyz.doikki.videocontroller.component.ErrorView;
 import xyz.doikki.videocontroller.component.PrepareView;
-import xyz.doikki.videoplayer.player.AndroidMediaPlayerFactory;
-import xyz.doikki.videoplayer.player.VideoViewConfig;
-import xyz.doikki.videoplayer.player.VideoViewManager;
 
-public class Monitoring extends AppCompatActivity {
 
+public class Monitoring extends AppCompatActivity implements OnTabSelectListener , View.OnClickListener {
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
     private static ActivityMonitoringBinding binding;
+    private String[] mTitles = {"行为统计", "图表分析"};
+    private MyPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +47,10 @@ public class Monitoring extends AppCompatActivity {
         ImmersionBar.with(this)
                 .fitsSystemWindows(true)  //使用该属性,必须指定状态栏颜色
                 .statusBarDarkFont(true, 0f)
-                .statusBarColor(R.color.white)
+                .statusBarColor(R.color.black)
                 .init();
+
+
         binding.player.setUrl("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"); //设置视频地址
 
         StandardVideoController controller = new StandardVideoController(this);
@@ -49,6 +63,16 @@ public class Monitoring extends AppCompatActivity {
         controller.addControlComponent(new ErrorView(this)); //错误界面
         binding.player.setVideoController(controller); //设置控制器
         binding.player.start(); //开始播放，不调用则不自动播放
+
+        mFragments.add(Daily.getInstance("行为统计"));
+        mFragments.add(Weekly.getInstance("图表分析"));
+        ViewPager vp = findViewById(R.id.vp);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        vp.setAdapter(mAdapter);
+        SlidingTabLayout tabLayout = findViewById(R.id.tl);
+        tabLayout.setViewPager(vp);
+        vp.setCurrentItem(0);
+
 
     }
 
@@ -75,6 +99,41 @@ public class Monitoring extends AppCompatActivity {
     public void onBackPressed() {
         if (!binding.player.onBackPressed()) {
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onTabSelect(int position) {
+    }
+
+    @Override
+    public void onTabReselect(int position) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+        }
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
         }
     }
 }
